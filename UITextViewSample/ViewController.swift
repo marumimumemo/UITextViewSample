ba2ff31cd18bae749617928028508fa32e385574
satoshi.marumoto <satoshi.marumoto@st-ventures.jp>
2020-03-31 18:13:41 +0900

    Initial Commit

---
+++ UITextViewSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UITextViewSample
+//
+//  Created by satoshi.marumoto on 2020/03/31.
+//  Copyright © 2020 satoshi.marumoto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
